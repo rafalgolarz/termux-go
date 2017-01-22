d306f5715217d50e370fee94d56f76be8f3e2091
Rafał Golarz <rafalgolarz@users.noreply.github.com>
2017-01-22 03:15:20 +0100

    Add files via upload
    An example of checking the battery status on Android phones via Termux API.
    It's a part of the article that can be found: http://rafalgolarz.com/blog/2017/01/15/running_golang_on_android/

---
+++ battery.go
@@ -0,0 +1,46 @@
+/**
+ * @Author Rafal Golarz
+ * http://rafalgolarz.com/blog/2017/01/15/running_golang_on_android/
+ */
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+var Battery struct {
+	Health      string
+	Percentage  int
+	Status      string
+	Temperature float64
+}
+
+func main() {
+	cmd := exec.Command("termux-battery-status")
+
+	//StdoutPipe returns a pipe that will be connected to
+	//the command's standard output when the command starts
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.NewDecoder(stdout).Decode(&Battery); err != nil {
+		log.Fatal(err)
+	}
+
+	//Wait waits for the command to exit
+	//It must have been started by Start
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The battery is %s and is charged in %d percentage\n",
+		Battery.Health, Battery.Percentage)
+}
